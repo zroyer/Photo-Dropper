8c339f93fdb71064da0d1a4c274e38d8d8ed04b7
Zach Royer <zroyer@gmail.com>
2015-03-29 14:46:34 -0500

    client basic functionality complete

--- client/drag-and-drop.js
+++ client/drag-and-drop.js
@@ -1,9 +1,34 @@
 var el = document.getElementById('dropzone')
 
-el.addEventListener("drop", function(event) {
+el.addEventListener("drop", function (event) {
 
 	event.preventDefault()
 	event.stopPropagation()
 
+
 	var file = event.dataTransfer.files[0]
+
+	var data = new FormData()
+	data.append('file', file)
+	data.append('fileName', file.name)
+	data.append('type', file.type)
+	
+	$.ajax({
+		url: '/image',
+		data: data,
+		processData: false,
+		contentType: false, 
+		type: 'POST',
+		success: function(data){
+			console.log('Photo dropped!')
+		}
+	})
+}, false)
+
+el.addEventListener('dragover', function (event) {
+	event.stopPropagation()
+	event.preventDefault()
+	event.dataTransfer.dropEffect = 'copy'
 })
+
+
--- client/socket-handler.js
+++ client/socket-handler.js
@@ -1,1 +1,13 @@
-socket-handler.js
+var socket = io.connect('http://localhost:8000')
+
+socket.on('Image:update', function (image) {
+  var reader = new FileReader()
+  reader.onload = function(e) {
+    $('#images')
+      .html('<img src="' + e.target.result + '" />')
+  }.bind(this);
+  reader.readAsDataURL(new Blob([image.file]))
+});
+
+
+
