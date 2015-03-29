23c21dbb31da3a72b914855793398b70c999171c
Zach Royer <zroyer@gmail.com>
2015-03-29 12:21:15 -0500

    building up client

--- client/drag-and-drop.js
+++ client/drag-and-drop.js
@@ -1,1 +1,9 @@
-drag-and-drop.js
+var el = document.getElementById('dropzone')
+
+el.addEventListener("drop", function(event) {
+
+	event.preventDefault()
+	event.stopPropagation()
+
+	var file = event.dataTransfer.files[0]
+})
